eb770c29b6376f0a2d4ebde339dc92b780f59122
lryain <liuhu_0618@sina.com>
2024-09-05 17:25:37 +0800

    l01 init commit

---
+++ l01/src/SerialNetTools/build/CMakeFiles/serialNetTools_test.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lryain/embeded-linux-dev/l01/src/SerialNetTools/main_test.cpp" "CMakeFiles/serialNetTools_test.dir/main_test.cpp.o" "gcc" "CMakeFiles/serialNetTools_test.dir/main_test.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ l01/src/SerialNetTools/build/CMakeFiles/serialNetTools_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/serialNetTools_test.dir/main_test.cpp.o"
+  "CMakeFiles/serialNetTools_test.dir/main_test.cpp.o.d"
+  "serialNetTools_test"
+  "serialNetTools_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/serialNetTools_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
