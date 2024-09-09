6c022960a58fba05242942216aa265a6e754b740
lryain <47129927@qq.com>
2024-09-09 20:55:35 +0800

    l08 init commit

---
+++ l08/Readme.md
@@ -0,0 +1,8 @@
+# l08
+
+项目更目录结构
+project
+    bin         可执行文件放到bin目录
+    build       编译文件放到build目录
+    lib         编译的库文件放到lib目录
+    logs        生成的日志文件放到lib目录
---
+++ l08/src/qt_ui/mainwindow.h
@@ -0,0 +1,39 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QString>
+#include <QDebug>
+#include "rosNode.h"
+#include "common.h"
+#include "myLog.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(int argc, char** argv, QWidget *parent = nullptr);
+    ~MainWindow();
+
+public slots:
+    void slot_btn_send();
+    void slot_send_textEdit(const QString&);
+
+signals:
+    void signal_send(const QString&);
+    void signal_rosPub(const QString&);
+
+private:
+    Ui::MainWindow *ui_;
+    RosNode rosNode_;
+    Common common_log_;
+    MyLog *myLog_;
+};
+#endif // MAINWINDOW_H
